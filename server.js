a5c36d5699270b50cc05dd3d5ef5d9cb108cbe3e
Daniel Ji <daniel.ji0923@gmail.com>
2023-12-13 19:19:57 -0800

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
+//**************************************************************************************//
 // Import in express
 //**************************************************************************************//
 const express = require("express")
 // Create app object
 const app = express()
 
-
+//**************************************************************************************//
 // Middleware
+//**************************************************************************************//
 
 
+
+
+//**************************************************************************************//
 // Routes
+//**************************************************************************************//
+
+// GREETINGS
 app.get("/greeting/:name", (req,res) => {
     const name = req.params.name
     res.send(`What's up, ${name}! It's so great to see you!`)
 })
 
+
+// TIP CALCULATOR
 app.get("/tip/:total/:tipPercentage", (req,res) => {
     const total = req.params.total
     const tipPercentage = req.params.tipPercentage
@@ -22,6 +32,14 @@ app.get("/tip/:total/:tipPercentage", (req,res) => {
 })
 
 
+// MAGIC 8 BALL
+app.get("/magic/Will%20I%20Be%20A%20Millionaire", (req,res) => {
+    const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const answer = answers[Math.floor(Math.random() * answers.length)]
+
+    res.send(`<h1>${answer}</h1>`)
+})
+//**************************************************************************************//
 // SERVER LISTENER
 //**************************************************************************************//
 app.listen(3000, () => {
